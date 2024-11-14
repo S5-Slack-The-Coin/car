add755a44326169f018f5a4b626b0c90e2acfcd0
DannickB <bild2707@usherbrooke.ca>
2024-11-14 14:03:11 -0500

    control update

--- s5i-picar_etudiants/S5i-PiCar_etudiants/control.py
+++ s5i-picar_etudiants/S5i-PiCar_etudiants/control.py
@@ -2,6 +2,9 @@ from picar import front_wheels
 from picar import back_wheels
 import time
 import picar
+from ultrasonic_module import Ultrasonic_Avoidance
+from SunFounder_PiCar_S.example.SunFounder_Line_Follower import Line_Follower
+
 
 class Control():
     def __init__(self):
@@ -16,37 +19,65 @@ class Control():
         self.set_speed(0)
         self.set_angle(90)
 
+        self.UA = Ultrasonic_Avoidance(20)
+        self.buffer_size = 4  # Change this value to adjust filtering
+        self.buffer = [0] * self.buffer_size
+        self.buffer_index = 0
+
+        self.lf = Line_Follower.Line_Follower(references=[100, 100, 100, 100, 100])
+
+    def get_distance(self):
+        current_dist = (self.UA.distance() + 4) * 10
+        if current_dist > 2500: current_dist = 2500
+        self.buffer[self.buffer_index] = current_dist
+        print(f"Last dist: {self.buffer[self.buffer_index]}")
+        if self.buffer_index < self.buffer_size - 1:
+            self.buffer_index += 1
+        else:
+            self.buffer_index = 0
+        avg = sum(self.buffer) / self.buffer_size
+        print(f"Buffer avg: {avg}")
+        return avg
+
+    def get_line_position(self):
+        return self.lf.read_digital()
+
     def get_speed(self):
         return self.speed
-    
-    def test(self):
-        self.bw.forward()
-        for i in range(0,10):
-            self.bw.speed = i*10
-            time.sleep(0.1)
-        for i in range(10,1, -1):
-            self.bw.speed = i*10
-            time.sleep(0.1)
-        self.bw.backward()
-        for i in range(0,10):
-            self.bw.speed = i*10
-            time.sleep(0.1)
-        for i in range(10,1, -1):
-            self.bw.speed = i*10
-            time.sleep(0.1)
 
-    def set_speed(self, speed, useDelta = False):
-        if useDelta: self.speed += speed
-        else: self.speed = speed
-        self.bw.backward()
-        self.bw.speed = speed
+    def set_speed(self, speed, useDelta=False):
+        if (speed < 0):
+            self.bw.forward()  # Accel negative
+        else:
+            self.bw.backward()
+        percent_speed = self.convert_speed(speed)
+        print(percent_speed)
+        ## Non testé avec la formule de convertion mm/s -> % mais j'assume qu'on aura pas besoin du delta any way
+        if useDelta:
+            self.speed += percent_speed
+        else:
+            self.speed = percent_speed
+        # self.speed = percent_speed
+
+        self.bw.speed = percent_speed
+        return percent_speed
+
+    def convert_speed(self, x):
+        y = abs(x / 1000) * 358.81 + 12.45  # Équation de la droite
+        if y < 25:
+            y = 0
+        elif y > 100:
+            y = 100
+        return int(y)
 
     def get_angle(self):
         return self.angle
 
-    def set_angle(self, angle, useDelta = False):
-        if useDelta: self.angle += angle
-        else: self.angle = angle
+    def set_angle(self, angle, useDelta=False):
+        if useDelta:
+            self.angle += angle
+        else:
+            self.angle = angle
 
         if self.angle in self.angleTable:
             computedAngle = self.angleTable[self.angle]
@@ -65,34 +96,48 @@ class Control():
             return self.angleTable[135]
         else:
             for angleID in self.angleTable.keys():
-                if angleID < angle < angleID+15:
+                if angleID < angle < angleID + 15:
                     lower_bound = self.angleTable[angleID]
-                    upper_bound = self.angleTable[angleID+15]
-                    delta = (angle - angleID)/15
-                    return int(lower_bound + (upper_bound - lower_bound)*delta)
+                    upper_bound = self.angleTable[angleID + 15]
+                    delta = (angle - angleID) / 15
+                    return int(lower_bound + (upper_bound - lower_bound) * delta)
         print("Error in angle computing")
+
+    def bw_test(self):
+        self.bw.forward()
+        for i in range(0, 10):
+            self.bw.speed = i * 10
+            time.sleep(0.1)
+        for i in range(10, 1, -1):
+            self.bw.speed = i * 10
+            time.sleep(0.1)
+        self.bw.backward()
+        for i in range(0, 10):
+            self.bw.speed = i * 10
+            time.sleep(0.1)
+        for i in range(10, 1, -1):
+            self.bw.speed = i * 10
+            time.sleep(0.1)
 
 
 if __name__ == "__main__":
     ctrl = Control()
-    angle_test = False
-    ctrl.set_angle(90)
-    ctrl.set_speed(0)
-    time.sleep(2)
+    angle_test = True
+    while True:
+        ctrl.get_line_position()
+
     while angle_test:
-        
+        print("forward")
+        ctrl.set_speed(150)
+        time.sleep(2)
+        print("backward")
+        ctrl.set_speed(-150)
+        time.sleep(2)
+        ctrl.set_speed(0)
+
         # Testing angles
         for i in range(11):
-            angle = 10*i + 40
+            angle = 10 * i + 40
             print(f"(hard) Angle: {angle}, Computed angle: {ctrl.set_angle(angle)}")
             time.sleep(1)
 
-        print("")
-        print(f"(soft) Angle: {40}, Computed angle: {ctrl.set_angle(-90, True)}")
-        time.sleep(1)
-        print(f"(soft) Angle: {50}, Computed angle: {ctrl.set_angle(5, True)}")
-        time.sleep(1)
-        for i in range(9):
-            angle = 10*i + 60
-            print(f"(soft) Angle: {angle}, Computed angle: {ctrl.set_angle(10, True)}")
-            time.sleep(1)
